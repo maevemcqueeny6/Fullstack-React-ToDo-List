3a98fe369cfeb6cc098a95d72cc3cb5b23162722
Maeve McQ <maevemcqueeny6@gmail.com>
2019-09-16 11:51:33 -0400

    heroku mlab database

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-// module.exports = {
-//     mongoURI: 'mongodb://localhost/todolist'
-// }
-
 module.exports = {
-    mongoURI: 'mongodb+srv://maeve:BETTYBOOP@cluster0-jtjzu.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb://localhost/todolist'
 }
+
+// module.exports = {
+//     mongoURI: 'mongodb+srv://maeve:BETTYBOOP@cluster0-jtjzu.mongodb.net/test?retryWrites=true&w=majority'
+// }
