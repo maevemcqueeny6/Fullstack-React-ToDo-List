2e7bcb6d6f91d79efbfa7dca5cdd5257ee76ae9d
Maeve McQ <maevemcqueeny6@gmail.com>
2019-09-16 11:51:58 -0400

    heroku database stuff

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-module.exports = {
-    mongoURI: 'mongodb://localhost/todolist'
-}
-
 // module.exports = {
-//     mongoURI: 'mongodb+srv://maeve:BETTYBOOP@cluster0-jtjzu.mongodb.net/test?retryWrites=true&w=majority'
+//     mongoURI: 'mongodb://localhost/todolist'
 // }
+
+module.exports = {
+    mongoURI: 'mongodb://heroku_1d7hldn0:32dks3tbircd7tmi3gfml5dcf2@ds051635.mlab.com:51635/heroku_1d7hldn0'
+}
